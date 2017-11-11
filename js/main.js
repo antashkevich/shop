364467663e0484c883db765d804b47893dd45e01
Antashkevich <antoha_ale6@mail.ru>
2017-11-11 16:40:02 +0300

    mob-version

--- js/main.js
+++ js/main.js
@@ -13,5 +13,24 @@ $(document).ready(function(){
         $(".tab-content").not(tab).css("display", "none");
         $(tab).fadeIn();
     });
+
+    /*drop down menu*/
+    $(function(){
+  
+        $('li.dropdown > a').on('click',function(event){        
+            event.preventDefault()            
+            $(this).parent().find('ul').first().toggle(300);            
+            $(this).parent().siblings().find('ul').hide(200);
+            
+            $(this).parent().find('ul').mouseleave(function(){  
+                var thisUI = $(this);
+                $('html').click(function(){
+                    thisUI.hide();
+                    $('html').unbind('click');
+                });
+            });    
+            
+        });      
+    });
     
 });
